def8adf796086028340cdd4c7006c75b78821229
testsheethal <sheethal.d11@wipro.com>
2019-02-14 07:39:37 +0000

    final commit

---
+++ src/test/java/DemoSelenium/DemoSelenium/NewTest.java
@@ -0,0 +1,24 @@
+package DemoSelenium.DemoSelenium;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	//FailDemoSelenium test1 = new FailDemoSelenium();
+	//PassDemoSelenium test2= new PassDemoSelenium();
+	
+  @Test(priority=1)
+  public void script1() {
+	  
+	  
+	 
+	  
+  }
+  
+  @Test(priority=2)
+  
+  public void script2()
+   {
+   
+   }
+}
---
+++ src/test/java/DemoSelenium/DemoSelenium/PassDemoSelenium.java
@@ -0,0 +1,43 @@
+package DemoSelenium.DemoSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.FindElement;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class PassDemoSelenium {    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+        WebDriver driver = null;
+       
+        @Test
+        public void verifyHomepageTitlePassTestCase() {
+             
+            System.out.println("launching Chrome browser"); 
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\d.d\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get(baseUrl);
+            String actualTitle = driver.getTitle();
+            Assert.assertEquals(actualTitle, expectedTitle);
+            driver.close();
+        }
+        
+        @Test
+        public void verifyHomepageTitleFailTestCase() {
+             
+            System.out.println("launching Chrome browser"); 
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\d.d\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get(baseUrl);
+            String actualTitle = driver.getTitle();
+            Assert.assertEquals(actualTitle, expectedTitle);
+            driver.findElement(By.name("null")).sendKeys("Test");
+            driver.close();
+        }
+    }
